11c3a7b4909fa07d54481db3cb00cab79c205663
varun <varun.chandran912203@gmail.com>
2024-07-10 22:05:41 +0530

    feat: Added new constraint to user edit request

--- backend/src/main/java/com/example/cablink/request_model/UserEdit.java
+++ backend/src/main/java/com/example/cablink/request_model/UserEdit.java
@@ -2,12 +2,15 @@ package com.example.cablink.request_model;
 
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.math.BigInteger;
 
 public record UserEdit (
         @NotBlank
+        @NotEmpty
         String name,
+
         @Digits(integer = 10, fraction = 0)
         BigInteger phNo
 ) {
