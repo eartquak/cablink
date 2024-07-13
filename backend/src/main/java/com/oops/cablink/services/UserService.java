1d81aeb247d765b15f8df46917d44c95f1ae06c4
varun <varun.chandran912203@gmail.com>
2024-07-13 16:27:24 +0530

    refactor: Auth renamed to UserService

--- backend/src/main/java/com/oops/cablink/services/UserService.java
+++ backend/src/main/java/com/oops/cablink/services/UserService.java
@@ -1,9 +1,10 @@
-package com.oops.cablink;
+package com.oops.cablink.services;
 
 import com.oops.cablink.models.User;
 import com.oops.cablink.repositories.UserRepository;
 import com.oops.cablink.response.GenericResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.Objects;
 
 @Component
-public class Auth {
+public class AuthService {
 
     @Autowired
     UserRepository userRepository;
@@ -23,14 +24,14 @@ public class Auth {
 
         if (principal.getAttribute("email") == null || Objects.requireNonNull(principal.getAttribute("email")).toString().isEmpty() || Objects.requireNonNull(
                 principal.getAttribute("email")).toString().isBlank()) {
-            return new GenericResponse("Error", GenericResponse.ResponseStatus.ERROR);
+            return new GenericResponse("Error", HttpStatus.UNAUTHORIZED);
         }
 
         final User currentUser = userRepository.findByEmail(Objects.requireNonNull(principal.getAttribute("email")).toString()).blockFirst();
         if (currentUser == null) {
-            return new GenericResponse("Could not find user", GenericResponse.ResponseStatus.ERROR);
+            return new GenericResponse("Could not find user", HttpStatus.UNAUTHORIZED);
         }
 
-        return new GenericResponse(currentUser, GenericResponse.ResponseStatus.SUCCESS);
+        return new GenericResponse(currentUser, HttpStatus.OK);
     }
 }
